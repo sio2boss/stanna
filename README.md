e71b840dda0210ff5d25a532b0d3599fea8c0411
sio2boss <sio2boss@hotmail.com>
2019-01-14 23:58:53 -0500

    Populating the message for you and at-mentioning

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # stanna
 From the Swedish verb 'to halt'.  Halt your machine after sending notification and waiting specified time limit.  This capability becomes useful when using cloud resources like Amazon EC2 where you are charged each hour the compute instance is running, regardless if it is doing any computation.  So say you wrote a bash script to do some amazing deep learning training or downloaded a bunch of data to be processed later.  Stanna allows you to do something like this:
 
-    ./do-awesome-stuff.sh && stanna "Awesome stuff done, $(hostname) shutting down."
+    ./do-awesome-stuff.sh; stanna
 
 Rather than having to setup email smarthost and all that 70's stuff.  You can create a channel in Slack, and then a [webhook in Slack](https://api.slack.com/incoming-webhooks).  So what happens in the above example is:
 
  1. ./do-awesome-stuff.sh does its thing.
- 2. the '&&' says if and only if ./do-awesome-stuff.sh exits successfully run stanna
- 3. stanna posts the message to Slack
+ 2. the ';' says when ./do-awesome-stuff.sh exits, run stanna
+ 3. stanna post to Slack
  4. waits the configured timeout (just in case you want to login and `stanna abort` the shutdown)
  5. system is haulted.
+ 6. you save $$$ in the cloud
 
 A simple configuration looks like this:
 
@@ -17,7 +18,8 @@ A simple configuration looks like this:
     {
       "waitTimeBeforeHalt": "500",
       "channel": "#deep-learning",
-      "webhook": "https://hooks.slack.com/services/asdfasdfasdf/asdfasdfasdf"
+      "webhook": "https://hooks.slack.com/services/asdfasdfasdf/asdfasdfasdf",
+      "user": "@awesome1"
     }
 
 You can create this file yourself or simply run the wizard:
--- index.js
+++ index.js
@@ -18,18 +18,87 @@ Usage:
   stanna --help
   stanna init
   stanna abort
-  stanna [--test] <message>
+  stanna [--test]
 Example, try:
   stanna "Just finished what you asked me todo, shutting myself down --computer"
 Options:
   -h, --help    Show this screen.
   --version     Show version.
   --test        Use to send slack message but not actually shutdown the machine
-  <message>     A double quoted message that will be sent to slack along with annotations
 `
 const args = neodoc.run(usage)
 configPath = getUserHome() + '/.stanna.json'
 
+// Messages
+var messages = [
+  "Yo! Your app is done doing what it was doing.",
+  "All wrapped up, just going to shutdown.",
+  "What do know? Your process completed, congrats!",
+  "It ain't your birthday but goin' to blow out these candles.",
+  "What the problem is?",
+  "Can't see it from my house.",
+  "Brainbox here: Requesting R&R.",
+  "Don't care, I'm getting some rest.",
+  "Let's blow this popstand.",
+  "Ding dong, it's King Kong smashing the gong!",
+  "Good night and good luck.",
+  "Ring a ding ding, check on this thing.",
+  "My app goes to yoga. Your app ... fruit rollup.",
+  "Who you gonna call?!",
+  "Stay classy San Diego.",
+  "Catch you on the rebound.",
+  "Catch you on the flip side.",
+  "It has been emotional, bye now.",
+  "Nice job breaking it, hero.",
+  "Yesterday I saw a deer.",
+  "Congratulations, the test is now over.",
+  "So. How are you holding up?",
+  "You look great, by the way. Very healthy.",
+  "This next test involves turrets.",
+  "Look, metal ball, I CAN hear you.",
+  "Let’s see what the next test is.",
+  "I've seen you in blue, I've seen you in yellow, but only you red will do for this fellow.",
+  "You are the fruit to my loom.",
+  "Red solo cup. I fill you up. Let's have a party!",
+  "Red solo cup. I lift you up. Proceed to party!",
+  "Yeah........I'm gonna need you to come in on Saturday.",
+  "I'm gonna have to disagree with you there.",
+  "Excuse me, but I think you have my stapler.",
+  "Didn't you get the memo?",
+  "I could set the building on fire.",
+  "I'm not going to do anything illegal.",
+  "It's not that I'm lazy, I just don't care.",
+  "He who controls the battlefield, controls history.",
+  "I’m no hero. Never was, never will be.",
+  "I don't have any more tears to shed.",
+  "I've never fought for anyone but myself.",
+  "A name means nothing on the battlefield.",
+  "We are already pulled over!",
+  "Can't pull over any farther!",
+  "I sure as heckfire remember you!",
+  "OK, campers, rise and shine, and don't forget your booties cause it's cold out there!",
+  "Don't mess with me, pork chop.",
+  "Did he actually refer to himself as 'the talent'?",
+  "You can't go. All the plants are gonna die.",
+  "Have that removed.",
+  "Do you think I'm officer material?",
+  "I'm worried about you.",
+  "We've got each other.",
+  "I don't think I've ever been this happy.",
+  "That's a fact, Jack.",
+  "You still have your health.",
+  "You could join a monastery.",
+  "Well sir, we were going to this bingo parlor at the YMCA...",
+  "And then depression set in.",
+  "Something like 8% of kids do it, but whatever.",
+  "Chop chop, lollipop!",
+  "Once more unto the breach, dear friends!",
+  "Peace out, girl scout!"
+]
+
+function getRandomMessage() {
+  return messages[Math.floor(Math.random()*messages.length)]
+}
 
 // Check for configuration -----------------------------------------------------------
 function getUserHome() {
@@ -40,13 +109,7 @@ function getUserHome() {
 // Shutdown procedure -----------------------------------------------------------
 function stanna(config, slack, args) {
 
-  if (!args['<message>']) {
-    console.log(chalk.red("Message not provided...exiting"))
-    console.log(usage)
-    exit(1)
-  }
-
-  var message = args['<message>']
+  var message = '<' + config.user + '> ' + getRandomMessage()
   var fields = [
     {
       title: 'Hostname',
@@ -123,40 +186,51 @@ function wizard(configPath) {
   console.log()
   console.log(chalk.bold('Setup Wizard'))
   config = {}
-  rl.question('How long would you like to delay the shutdown in seconds: ', (answer) => {
+  rl.question('What is your slack handle? (e.g. @awesome1): ', (answer) => {
 
-    // Timeout
-    console.log(chalk.gray('Setting delay to', answer, 'seconds'))
-    config.waitTimeBeforeHalt = answer
+    // User
+    if (answer.length > 1 && answer[0] !== '@') {
+      answer = '@' + answer
+    }
+    console.log(chalk.gray('Setting username to', answer))
+    config.user = answer
 
-    rl.question('What channel would you like to post messages to: ', (channel) => {
+    rl.question('How long would you like to delay the shutdown in seconds: ', (answer) => {
 
-      // Channel
-      console.log(chalk.gray('Setting channel to', channel))
-      config.channel = channel
+      // Timeout
+      console.log(chalk.gray('Setting delay to', answer, 'seconds'))
+      config.waitTimeBeforeHalt = answer
 
-      rl.question('Need a slack webhook URI: ', (webhook) => {
+      rl.question('What channel would you like to post messages to: ', (channel) => {
 
-        // Webhook
-        console.log(chalk.gray('Setting webhook to', webhook))
-        config.webhook = webhook
-        rl.close()
+        // Channel
+        console.log(chalk.gray('Setting channel to', channel))
+        config.channel = channel
 
-        // Save to disk and exit
-        console.log('Writing configuration settings to: ', configPath)
-        fs.open(configPath, 'w+', function(err, f) {
-          if (err) {
-              console.log("ERROR !! " + err)
-              exit(1)
-          }
+        rl.question('Need a slack webhook URI: ', (webhook) => {
 
-          fs.writeFile(f, JSON.stringify(config), function(err) {
-            if(err) {
-              console.log(err)
-              exit(1)
+          // Webhook
+          console.log(chalk.gray('Setting webhook to', webhook))
+          config.webhook = webhook
+          rl.close()
+
+          // Save to disk and exit
+          console.log('Writing configuration settings to: ', configPath)
+          fs.open(configPath, 'w+', function(err, f) {
+            if (err) {
+                console.log("ERROR !! " + err)
+                exit(1)
             }
 
-            exit(0)
+            fs.writeFile(f, JSON.stringify(config), function(err) {
+              if(err) {
+                console.log(err)
+                exit(1)
+              }
+
+              exit(0)
+
+            })
 
           })
 
